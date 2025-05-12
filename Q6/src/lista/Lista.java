d6eb89472f04caa29d0c9cb7a026ad6cefa09e59
Marcos Vinicius <157753545+MVLRGTR@users.noreply.github.com>
2025-05-12 19:15:38 -0300

    useLista parcial

--- Q6/src/lista/Lista.java
+++ Q6/src/lista/Lista.java
@@ -19,7 +19,7 @@ public class Lista {
 	
 	public void inserirPrimeiro(int valor) {
 		No novo = new No(valor);
-		if(this.primeiro == null) {
+		if(this.estaVazio()) {
 			this.primeiro=novo;
 			this.atual=novo;
 			this.ultimo=novo;
@@ -27,18 +27,20 @@ public class Lista {
 			this.menor=novo.valor;
 			this.vtotal+=valor;
 			this.tamanho+=1;
-			this.maiorEmenor(valor);
 			this.imparEpar(valor);
 		}else {
 			novo.proximo=this.primeiro;
 			this.primeiro=novo;
 			this.vtotal+=valor;
+			this.tamanho+=1;
+			this.imparEpar(valor);
+			this.maiorEmenor(valor);
 		}
 	}
 	
 	public void inserirUltimo(int valor) {
 		No novo = new No(valor);
-		if(this.primeiro == null) {
+		if(this.estaVazio()) {
 			this.primeiro=novo;
 			this.atual=novo;
 			this.ultimo=novo;
@@ -46,12 +48,135 @@ public class Lista {
 			this.menor=novo.valor;
 			this.vtotal+=valor;
 			this.tamanho+=1;
-			this.maiorEmenor(valor);
 			this.imparEpar(valor);
 		}else {
-			novo.proximo=this.primeiro;
-			this.primeiro=novo;
+			this.ultimo.proximo=novo;
+			this.ultimo=novo;
 			this.vtotal+=valor;
+			this.tamanho+=1;
+			this.imparEpar(valor);
+			this.maiorEmenor(valor);
+		}
+	}
+	
+	public void inserirNaPosicao(int posicao,int valor) {
+		if(posicao > 1 && posicao <= this.tamanho()) {
+			No novo = new No(valor);
+			this.irPosicao(posicao-2);
+			novo.proximo = this.atual.proximo;
+			this.atual.proximo = novo;
+			this.vtotal+=valor;
+			this.tamanho+=1;
+			this.imparEpar(valor);
+			this.maiorEmenor(valor);
+		}else if(posicao == 1) {
+			this.inserirPrimeiro(valor);
+		}else if(posicao == this.tamanho()) {
+			this.inserirUltimo(valor);
+		}
+	}
+	
+	public No primeiroElemento() {
+		return this.primeiro;
+	}
+	
+	public No ultimoElemento() {
+		return this.ultimo;
+	}
+	
+	public No elementoNaPosicao(int posi) {
+		if(posi <= this.tamanho()) {
+			this.irPosicao(posi);
+			return atual;
+		}
+		return null;
+	}
+	
+	public void removePrimeiro() {
+		this.primeiro=this.primeiro.proximo;
+		this.tamanho-=1;
+		this.atualizarParametros();
+	}
+	
+	public void removeUltimo() {
+		this.irPosicao(this.tamanho()-2);
+		this.ultimo = this.atual;
+		this.atual.proximo = null;
+		this.tamanho-=1;
+		this.atualizarParametros();
+	}
+	
+	public void removeNaPosicao(int posi) {
+		if(posi == 1) {
+			this.removePrimeiro();
+		}else if(posi < this.tamanho()){
+			this.irPosicao(posi-1);
+			No temp =  this.atual.proximo;
+			this.irPosicao(posi-2);
+			this.atual.proximo=temp;
+			this.tamanho-=1;
+			this.atualizarParametros();
+		}else if(posi == this.tamanho()) {
+			this.removeUltimo();
+		}
+	}
+	
+	public void removeElemento(int elemento) {
+		int posi = this.buscaElemento(elemento);
+		if(posi > -1) {
+			this.irPosicao(posi);
+			No temp = atual.proximo;
+			this.irPosicao(posi-1);
+			this.atual.proximo=temp;
+			this.tamanho-=1;
+			this.atualizarParametros();
+		}
+	}
+	
+	public int buscaElemento(int v) {
+		int cont=0;
+		this.atual = this.primeiro;
+		
+		while(this.atual != null && this.atual.valor != v) {
+			cont++;
+			this.atual = this.atual.proximo;
+		}
+		
+		if(this.atual != null) {
+			return cont;
+		}else {
+			return -1;
+		}
+		
+	}
+	
+	private void irPosicao(int posicao) {
+		if(posicao <= this.tamanho()) {
+			this.atual = this.primeiro;
+			for(int i = 0 ; i < posicao;i++) {
+				this.atual = this.atual.proximo;
+			}
+		}else {
+			this.atual=this.primeiro;
+		}
+	}
+	
+	public int tamanho() {
+		int cont = 0;
+		No aux = this.primeiro;
+		while(aux!=null) {
+			cont++;
+			aux=aux.proximo;
+		}
+		return cont;
+	}
+	
+	public boolean estaVazio() {
+		if(this.primeiro == null) {
+			this.ultimo=null;
+			return true;
+		}else {
+			return false;
 		}
 	}
 	
@@ -69,6 +194,50 @@ public class Lista {
 		}else if(valor < this.menor){
 			this.menor=valor;
 		}
+	}
+
+	public int getMaior() {
+		return maior;
+	}
+
+	public int getMenor() {
+		return menor;
+	}
+
+	public int getPar() {
+		return par;
+	}
+
+	public int getImpar() {
+		return impar;
+	}
+	
+	public int vtotal() {
+		return this.vtotal;
+	}
+	
+	public float media() {
+		return this.vtotal/this.tamanho;
+	}
+	
+	private void atualizarParametros() {
+		int cont=0;
+		this.atual = this.primeiro;
+		this.vtotal=0;
+		this.par=0;
+		this.impar=0;
+		
+		while(this.atual != null) {
+			if(cont==0) {
+				this.maior=this.atual.valor;
+				this.menor=this.atual.valor;
+			}
+			cont++;
+			this.maiorEmenor(this.atual.valor);
+			this.imparEpar(this.atual.valor);
+			this.atual = this.atual.proximo;
+		}
+		
 	}
 	
 }
--- Q6/src/progam/UseLista.java
+++ Q6/src/progam/UseLista.java
@@ -1,9 +1,41 @@
 package progam;
 
+import lista.Lista;
+
 public class UseLista {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Lista lista1 = new Lista();
+		Lista lista2 = new Lista();
+		
+		System.out.println("Colocando dados na lista1...");
+		lista1.inserirPrimeiro(1);
+		lista1.inserirUltimo(2);
+		lista1.inserirUltimo(3);
+		lista1.inserirUltimo(4);
+		lista1.inserirUltimo(5);
+		lista1.inserirUltimo(6);
+		
+		System.out.println("Colocando dados na lista2...");
+		lista2.inserirPrimeiro(10);
+		lista2.inserirUltimo(11);
+		lista2.inserirUltimo(12);
+		lista2.inserirUltimo(13);
+		lista2.inserirUltimo(14);
+		lista2.inserirUltimo(15);
+		
+		if (lista1.tamanho() == lista2.tamanho() && lista1.vtotal() == lista2.vtotal()) {
+			System.out.printf("As duas listas são iguais !!! tamanho das listas é : %d com valor total de  : %d",
+					lista1.tamanho(), lista1.vtotal());
+		} else {
+			if (lista1.vtotal() > lista2.vtotal()) {
+				System.out.println("A lista1 e maior que a da lista2!!!");
+			} else if (lista1.vtotal() == lista2.vtotal()) {
+				System.out.println("As duas listas tem o mesmo valor total de soma !!!");
+			} else {
+				System.out.println("A lista2 é maior que a lista1!!!");
+			}
+		}
 
 	}
 
